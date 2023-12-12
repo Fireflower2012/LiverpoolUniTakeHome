7218b75bb8447db0b2ce5d62279e5e5c73c854a6
fireflower2012@gmail.com <fireflower2012@gmail.com>
2023-12-12 23:03:47 +0000

    Minor display improvements

--- LiverpoolUnitTakeHome/Models/Student.cs
+++ LiverpoolUnitTakeHome/Models/Student.cs
@@ -5,14 +5,17 @@ namespace LiverpoolUnitTakeHome.Models
     public class Student
     {
         public int ID { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Surname")]
         public string LastName { get; set;}
+        [Display(Name = "Date Of Birth")]        
+        //TODO validation to make sure dob is earlier than today
         public DateOnly DateOfBirth { get; set; }
         [Range(1,3)]
+        [Display(Name = "Year Of Study")]
         public int YearOfStudy { get; set; }
         public ICollection<StudentSubject> StudentSubject { get; set; } = new List<StudentSubject>();
-
-     //   public ICollection<Subjects> Subject { get; set; } = new List<Subjects>();
 
     }
 }
